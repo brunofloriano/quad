c77ca494fc04d2751aaeb80a9733634c84190a41
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-10 10:20:46 -0300

    Acel_funcao5.3

--- main/medicao.cpp
+++ main/medicao.cpp
@@ -1,3 +1,9 @@
+#include "include/command.h"
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+#include <time.h>
 #include "medicao.h"
 
 int medicao(clock_t tsim, clock_t tam){
