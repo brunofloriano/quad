b35789b6f62d19de3e649ef0c9080343cd2dcd16
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-14 14:38:07 -0300

    Acel_teste6.1.6

--- main/medicao6.1.cpp
+++ main/medicao6.1.cpp
@@ -1,13 +1,3 @@
-#include "include/command.h"
-#include <unistd.h>     // UNIX standard function definitions
-#include <fcntl.h>      // File control definitions
-#include <errno.h>      // Error number definitions
-#include <termios.h>    // POSIX terminal control definitions
-
-#ifndef PI
-#define PI    3.14159265
-#endif // PI
-
 using namespace std;
 
 int medicao_angulos(float *angulos){
