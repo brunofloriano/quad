9b0cbc566f00bc168d359518d66b0c195bdf7f61
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-15 14:47:50 -0300

    Acel_teste8.2

---
+++ main/filtro.cpp
@@ -0,0 +1,10 @@
+float filtro(float tam, float fc, float ak){
+    float pi = 3.1415926;
+    float T = tam/1000; //tempo de amostragem em segundos
+    float a1= 1/(1+T*2*pi*fc), a2 = T*2*pi*fc/(1+T*2*pi*fc);
+    static float Afk = 0;
+
+    Afk = a1*Afk + a2*Ak;
+
+return Afk;
+}
