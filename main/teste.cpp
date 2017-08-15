c7a21e57cd99301f007b1b51986cc4472a68f020
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-15 08:32:09 -0300

    Acel_teste6.1.13

--- main/teste.cpp
+++ main/teste.cpp
@@ -8,12 +8,15 @@ using namespace std;
 
 int main(){
 
+    float angulos[2];
+    float roll, pitch;
+
     medicao_angulos(angulos);
     roll = angulos[0];
     pitch = angulos[1];
 
     cout << roll << " " << pitch << " \n" <<endl;
-}
+
 
 return 0;
 }
