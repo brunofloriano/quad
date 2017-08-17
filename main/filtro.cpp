a452fb6ba3e2d10ac62bb7ddeae3d3e0fcb7542b
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-17 11:12:14 -0300

    Filtro

--- main/filtro.cpp
+++ main/filtro.cpp
@@ -4,6 +4,8 @@ float filtro(float tam, float fc, float Ak, float Afk){
     float T = tam/1000; //tempo de amostragem em segundos
     float a1= 1/(1+T*2*pi*fc), a2 = T*2*pi*fc/(1+T*2*pi*fc);
 
+    printf("%f %f %f %f ", tam,fc,Ak,Afk);
+
     Afk = a1*Afk + a2*Ak;
 
 return Afk;
