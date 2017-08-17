8784e2c9c65d6e8c3f398348723e3abb4d858097
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-17 15:28:14 -0300

    Filtro

--- main/acelerometro.c
+++ main/acelerometro.c
@@ -12,10 +12,10 @@ int main(){
     clock_t tam = 500; /*tempo de amostragem em milisegundos*/
     int contador2 = 0;
     float roll;
+    float pitch;
     float angulos[2];
     angulos[0] = 0;
     angulos[1] = 0;
-    float pitch;
     /*float fc = 1; frequencia de corte do filtro passa baixas*/
     char C;
     clock_t tInicio;
