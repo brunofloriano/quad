c536117abad6ec23db9023b62b9e06087fda4c66
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-17 14:57:21 -0300

    Filtro

--- main/acelerometro.c
+++ main/acelerometro.c
@@ -14,7 +14,7 @@ int main(){
     float angulos[2];
     angulos[0] = 0;
     angulos[1] = 0;
-    float roll
+    float roll;
     float pitch;
     float fc = 1; /*frequencia de corte do filtro passa baixas*/
     char C;
