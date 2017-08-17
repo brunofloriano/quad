c460bff2f803cb822b64d8b3c31f62b922388e9d
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-17 11:28:41 -0300

    Filtro

--- main/filtro.cpp
+++ main/filtro.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-float filtro(float tam, float fc, float Ak, float Afk){
+float filtro(float tam, float fc, float Afk, float Ak){
     float pi = 3.1415926;
     float T = tam/1000; //tempo de amostragem em segundos
     float a1= 1/(-1+T*2*pi*fc), a2 = T*2*pi*fc/(-1+T*2*pi*fc);
