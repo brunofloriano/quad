2edf94193f9c823a68ca547a804273367caec952
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-17 10:07:54 -0300

    Makefile

--- main/medicao_acel.cpp
+++ main/medicao_acel.cpp
@@ -76,8 +76,9 @@ int medicao_angulos(float *angulos){
             n_written = write( USB, "1", 1 );
 
             do
-            {
+            {   cout << "Ok1" << endl;
                 n = read( USB, &buf, 1 );
+                cout << "Ok2" << endl;
             }
             while( buf != '<' && n > 0);
             do
