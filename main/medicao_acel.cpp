c8b344f1bbf4baf752234ba4da5c681800f9f623
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-18 11:27:40 -0300

    Filtro

--- main/medicao_acel.cpp
+++ main/medicao_acel.cpp
@@ -77,14 +77,16 @@ int medicao_angulos(float *angulos){
 
             do
             {
-                n = read(USB, &buf, 1);
-            } while( (buf != '<') && (n > 0));
+                n = read( USB, &buf, 1 );
+            }
+            while( buf != '<' && n > 0);
             do
             {
-                n = read(USB, &buf, 1);
+                n = read( USB, &buf, 1 );
                 sprintf(&response[spot],"%c",buf);
                 spot += n;
-            } while( (buf != '>') && (n > 0));
+            }
+            while( buf != '>' && n > 0);
             temp=response;
             inic=temp.find('\n');
             fim=inic;
