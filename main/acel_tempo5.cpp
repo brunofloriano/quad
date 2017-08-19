94ff9e465837790e97fd486519178ad10213eec6
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-19 15:57:41 -0300

    Filtro

--- main/acel_tempo5.cpp
+++ main/acel_tempo5.cpp
@@ -11,17 +11,31 @@
 using namespace std;
 
 int main(){
-    clock_t tsim1 = 5; //tempo de simulacao em segundos
-    clock_t tam1 = 1000; //tempo de amostragem em microsegundos
+    clock_t tsim = 5; //tempo de simulacao em segundos
+    clock_t tam = 1000; //tempo de amostragem em microsegundos
+    clock_t tInicio, tFim, tDecorrido;
     float angulos[2];
     char X;
+    int contador2 = 0;
 
     int USB = inicializacao();
     cout << "\n Pressione qualquer tecla para iniciar \n" << endl;
     cin >> X;
 
+    tInicio = clock();
+    tFim = clock();
+    tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+    //Loop
+    while(tDecorrido<tsim*1000){
+	if(tDecorrido>contador2){
     inicializacao();
     medicao(angulos, tsim1,tam1,USB);
+    cout << angulos[0] << " " << angulos[1] << endl;
+    contador2 = contador2 + tam;
+    }
+	tFim = clock();
+	tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+}
 
 return 0;
 }
--- main/medicao.cpp
+++ main/medicao.cpp
@@ -60,21 +60,13 @@ int medicao(float *angulos, clock_t tsim, clock_t tam, int USB){
     char response[1024];
 
     int n = 0, n_endl, spot = 0, n_written = 0;
-    int contador2 = 0;
     float temp_val[7];
     float xAcel, yAcel, zAcel;
-
-    clock_t tInicio, tFim, tDecorrido;
 
     string temp,temp2;
     size_t inic, fim;
 
-    tInicio = clock();
-    tFim = clock();
-    tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
-    //Loop
-    while(tDecorrido<tsim*1000){
-	if(tDecorrido>contador2){
+
             memset(temp_val, 0, sizeof temp_val);
             n=0;
             spot=0;
@@ -121,12 +113,7 @@ int medicao(float *angulos, clock_t tsim, clock_t tam, int USB){
     angulos[1] = 3+atan(yAcel/(sqrt(xAcel*xAcel+zAcel*zAcel)))*180/PI; //pitch
 
 
-           cout << angulos[0] << " " << angulos[1] << endl;
-            contador2 = contador2 + tam;
-            }
-	tFim = clock();
-	tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
-}
+
 close(USB);
 return 0;
 }
