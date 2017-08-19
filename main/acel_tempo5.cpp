4fc262e69ba95f307fd27de1a0b81f7aa7315439
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-19 15:25:34 -0300

    Filtro

--- main/acel_tempo5.cpp
+++ main/acel_tempo5.cpp
@@ -14,8 +14,10 @@ int main(){
     clock_t tsim1 = 10; //tempo de simulacao em segundos
     clock_t tam1 = 500; //tempo de amostragem em microsegundos
     float angulos[2];
+    char X:
 
     cout << "\n Pressione qualquer tecla para iniciar \n" << endl;
+    cin >> X;
 
     int USB = inicializacao();
     medicao(angulos, tsim1,tam1,USB);
