0459d6587fbe388ce17d3b1163db5bd8ec9a4bde
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-22 14:37:36 -0300

    Teste_motores

---
+++ main/teste_motor.cpp
@@ -0,0 +1,52 @@
+#include "include/command.h"
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+
+#define Kp_roll 1
+#define Kd_roll 0
+#define Kp_pitch 1
+#define Kd_pitch 0
+#define BAUDRATE                        1000000				// Padrao p/este trabalho
+#define DEVICENAME                      "/dev/ttyUSB0"      // Usando o conversor USB
+#define dt			                    0.01
+#define TASK_PERIOD                     dt*1e9
+#define BROADCASTID			            254
+#define MOVING_SPEED                    80
+#define MAX_TORQUE                      1024
+#define TORQUE_LIMIT                    1024
+#define PI                              3.14159265
+
+int main(){
+
+    char *dev_name = (char*)DEVICENAME;
+    dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(1);
+    dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(dev_name);
+    dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, 30, 2);
+    uint8_t param_goal_position[2];
+
+
+    if (portHandler->openPort())
+    {
+        printf("Succeeded to open the port!\n\n");
+        printf(" - Device Name : %s\n", dev_name);
+        printf(" - Baudrate    : %d\n\n", portHandler->getBaudRate());
+    }
+    else
+    {
+        printf("Failed to open the port! [%s]\n", dev_name);
+        printf("Press any key to terminate...\n");
+        cmd.getch();
+        return 0;
+    }
+
+
+    cmd.write_torque(portHandler, packetHandler, BROADCASTID, 0);
+    cmd.config_ram(portHandler, packetHandler);
+    cmd.getch();
+
+    cmd.write_pos(portHandler, packetHandler, BROADCASTID, 500);
+
+return 0;
+}
