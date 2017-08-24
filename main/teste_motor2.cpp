61da155ddf40088778a32293b2a289c43caf993e
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-24 11:10:52 -0300

    Teste_Motores

--- main/teste_motor2.cpp
+++ main/teste_motor2.cpp
@@ -50,12 +50,13 @@ int main(){
     clock_t tsim = 10; //tempo de simulacao em segundos
     clock_t tam = 100; //tempo de amostragem em microsegundos
     clock_t tInicio, tFim, tDecorrido;
+    float tamf = tam;
     float out;
     float angulos[2];
     float roll_medido, pitch_medido, roll = 0, pitch = 0;
     float velocidade_roll, velocidade_pitch;
     float fc = 1;
-    float K1 = 10000,K4 = 1, K7 = 1, K10 = 1;    //roll gains
+    float K1 = 1,K4 = 1, K7 = 1, K10 = 1;    //roll gains
     int contador2 = 0;
     int USB = inicializacao();
 
@@ -100,10 +101,10 @@ int main(){
     pitch_medido = angulos [1];
 
     filtro(tam, fc, roll_medido, roll, &out);
-    velocidade_roll = (out - roll)*(PI/180)/(tam*1000);  //em rad/s
+    velocidade_roll = (out - roll)*(PI/180)/(tamf*1000);  //em rad/s
     roll = out;
     filtro(tam, fc, pitch_medido, pitch, &out);
-    velocidade_pitch = (out - pitch)*(PI/180)/(tam*1000);  //em rad/s
+    velocidade_pitch = (out - pitch)*(PI/180)/(tamf*1000);  //em rad/s
     pitch = out;
 
 
