3896b344bbf1ab9abc546594e7a2bd5600fbfe4d
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-24 10:00:12 -0300

    Teste_Motores

--- main/teste_motor2.cpp
+++ main/teste_motor2.cpp
@@ -4,6 +4,7 @@
 #include <errno.h>      // Error number definitions
 #include <termios.h>    // POSIX terminal control definitions
 #include "medicao.h"
+#include "filtro.cpp"
 #include <stdio.h>
 
 #define Kp_roll 1
@@ -54,7 +55,6 @@ int main(){
     float angulos[2];
     float roll_medido, pitch_medido, roll = 0, pitch = 0;
     float fc = 1;
-    char X;
     int contador2 = 0;
     int USB = inicializacao();
 
@@ -75,12 +75,32 @@ int main(){
         return 0;
     }
 
+
+    tInicio = clock();
+    tFim = clock();
+    tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+    //Loop
+    while(tDecorrido<tsim*1000){
+	if(tDecorrido>contador2){
+
+
     inicializacao();
     medicao(angulos, USB);
     roll_medido = angulos[0];
     pitch_medido = angulos [1];
 
+    filtro(tam, fc, roll_medido, roll, &out);
+    roll = out;
+    filtro(tam, fc, pitch_medido, pitch, &out);
+    pitch = out;
+
     printf("%f %f \n", roll_medido, pitch_medido);
+
+    contador2 = contador2 + tam;
+    }
+	tFim = clock();
+	tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+}
 
     cmd.config_ram(portHandler, packetHandler);
     cmd.getch();
