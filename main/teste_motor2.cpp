e4f567ec78c81ccf966e95298cd77cc7c38508e9
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-08-29 09:19:49 -0300

    Teste Motores

--- main/teste_motor2.cpp
+++ main/teste_motor2.cpp
@@ -182,7 +182,7 @@ int main(){
     v_medicao_int = cmd.read_mov_speed(portHandler, packetHandler, i);
     v_medicao = ler_velocidade(v_medicao_int);
     v_aplicada = v_desejada - v_medicao;
-    cmd.write_mov_speed(portHandler, packetHandler, i, velocidade(v_desejada));
+    cmd.write_mov_speed(portHandler, packetHandler, i, velocidade(v_aplicada));
 	i++;
     }
 	i = 1;
