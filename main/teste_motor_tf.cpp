2984a6640f7b92c74be38763ceb902059e41930b
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-09-05 10:42:28 -0300

    Teste Posicao Inicial

--- main/teste_motor_tf.cpp
+++ main/teste_motor_tf.cpp
@@ -161,7 +161,8 @@ int main(){
          while (read[i]!=write[i]){
             read[i] = cmd.read_pos(portHandler, packetHandler, i+1);
             diff = write[i]-read[i];
-            cmd.write_mov_speed(portHandler, packetHandler, i+1, diff);
+            if(diff<0){diff = abs(diff)+1024}
+            cmd.write_mov_speed(portHandler, packetHandler, i+1, velocidade(ler_velocidade(diff)));
 
          }
 
