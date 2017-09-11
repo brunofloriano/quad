884d7b535d69f1d76681f2817ff9717a9494b206
Bruno Rodolfo Oliveira Floriano <brunofloriano3@gmail.com>
2017-09-11 14:48:07 -0300

    Threads

--- main/main.cpp
+++ main/main.cpp
@@ -28,7 +28,7 @@ int main(){
     float tsim = 5; //tempo de simulacao em segundos
     pthread_t id;
     int i = 1;
-    long tid
+    long tid;
 
 
     printf("Pressione qualquer tecla para iniciar \n");
