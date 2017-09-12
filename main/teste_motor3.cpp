12ac3e7903216f4af4e88fd1635669984a3a7c43
Bruno Floriano <brunofloriano@github.com>
2017-09-12 09:58:45 -0300

    Threads

--- main/teste_motor3.cpp
+++ main/teste_motor3.cpp
@@ -3,7 +3,7 @@
 #include <fcntl.h>      // File control definitions
 #include <errno.h>      // Error number definitions
 #include <termios.h>    // POSIX terminal control definitions
-//#include "medicao.h"
+#include "medicao.h"
 #include "filtro.h"
 #include <stdio.h>
 
@@ -56,122 +56,18 @@ return v*CCW;
 
 }
 
-int inicializacao(){
+int main(){
 
-    int USB = open( "/dev/ttyACM0", O_RDWR| O_NOCTTY );
-    close(USB);
-    USB = open( "/dev/ttyACM0", O_RDWR| O_NOCTTY );
-
-    //USB Handling//
-    struct termios tty;
-    struct termios tty_old;
-    memset (&tty, 0, sizeof tty);
-
-    /* Error Handling */
-    if ( tcgetattr ( USB, &tty ) != 0 )
-    {
-        std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
-    }
-
-    /* Save old tty parameters */
-    tty_old = tty;
-    /* Set Baud Rate */
-    cfsetospeed (&tty, (speed_t)B1000000);
-    cfsetispeed (&tty, (speed_t)B1000000);
-    /* Setting other Port Stuff */
-    tty.c_cflag     &=  ~PARENB;            // Make 8n1
-    tty.c_cflag     &=  ~CSTOPB;
-    tty.c_cflag     &=  ~CSIZE;
-    tty.c_cflag     |=  CS8;
-    tty.c_cflag     &=  ~CRTSCTS;           // no flow control
-    tty.c_cc[VMIN]   =  1;                  // read doesn't block
-    tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
-    tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
-    /* Make raw */
-    cfmakeraw(&tty);
-    /* Flush Port, then applies attributes */
-    //tcflush( USB, TCIFLUSH );
-    if ( tcsetattr ( USB, TCSANOW, &tty ) != 0)
-    {
-        std::cout << "Error " << errno << " from tcsetattr" << std::endl;
-    }
-
-return USB;
-}
-
-int medicao(float *angulos, int USB){
-
-    command cmd;
-    char buf = '\0';
-    char response[1024];
-
-    int n = 0, n_endl, spot = 0, n_written = 0;
-    float temp_val[7];
-    float xAcel, yAcel, zAcel;
-
-    string temp,temp2;
-    size_t inic, fim;
-
-
-            memset(temp_val, 0, sizeof temp_val);
-            n=0;
-            spot=0;
-            buf='\0';
-            n_endl=0;
-            memset(response, '\0', sizeof response);
-
-            n_written = write( USB, "1", 1 );
-
-            do
-            {
-                n = read( USB, &buf, 1 );
-            }
-            while( buf != '<' && n > 0);
-            do
-            {
-                n = read( USB, &buf, 1 );
-                sprintf(&response[spot],"%c",buf);
-                spot += n;
-            }
-            while( buf != '>' && n > 0);
-            temp=response;
-            inic=temp.find('\n');
-            fim=inic;
-            while(fim!=string::npos)
-            {
-                fim=temp.find('\n',inic+1);
-                temp2=temp.substr(inic+1,fim-inic-1);
-                inic=fim;
-                if(n_endl<7)
-                {
-                    temp_val[n_endl]+=atof(temp2.c_str());
-                }
-                n_endl++;
-            }
-            tcflush( USB, TCIFLUSH );
-
-
-    xAcel = temp_val[1];
-    yAcel = -temp_val[0];
-    zAcel = temp_val[2];
-
-    angulos[0] = atan(-xAcel/zAcel)*180/PI;   //roll
-    angulos[1] = 3+atan(yAcel/(sqrt(xAcel*xAcel+zAcel*zAcel)))*180/PI; //pitch
-
-
-
-close(USB);
-return 0;
-}
-
-void controle(){
     command cmd;
     char *dev_name = (char*)DEVICENAME;
     dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(1);
     dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(dev_name);
     dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, 30, 2);
-    
-    
+
+
+    clock_t tInicio, tFim;
+    float tDecorrido;
+    float tsim = 10; //tempo de simulacao em segundos
     float tam = 100; //tempo de amostragem em milisegundos
     float out;
     float angulos[2];
@@ -183,13 +79,14 @@ void controle(){
     float K_pitch_F = 1.01, K_pitch_R = 1.01;
     float K_UP = 1, K_DOWN = -1;
     float K[13];
+    float contador2 = 0;
     float threshold = 0.02;
-    
+
+
     int i = 1;
     int v_medicao_int;
     int USB = inicializacao();
-    
-    
+
 
     K[0] = 0;
     //----------roll gains-------//
@@ -206,66 +103,6 @@ void controle(){
     K[9] = -K_pitch_F*K_DOWN;
     K[11] = K_pitch_F*K_UP;
     K[12] = K_pitch_F*K_DOWN;
-    
-    portHandler->openPort();
-    portHandler->getBaudRate();
-    cmd.config_ram(portHandler, packetHandler);
-    
-    
-    inicializacao();
-    medicao(angulos, USB);
-    roll_medido = angulos[0];
-    pitch_medido = angulos [1];
-
-    filtro(tam, fc, roll_medido, roll, &out);
-    velocidade_roll = (out - roll)*(PI/180)/(tam/1000);  //em rad/s
-    roll = out;
-    filtro(tam, fc, pitch_medido, pitch, &out);
-    velocidade_pitch = (out - pitch)*(PI/180)/(tam/1000);  //em rad/s
-    pitch = out;
-
-   if(abs(velocidade_roll)<threshold){velocidade_roll = 0;}
-   if(abs(velocidade_pitch)<threshold){velocidade_pitch = 0;}
-
-    printf("%f %f \n", velocidade_roll, velocidade_pitch);
-    i = 1;
-    while(i<13){
-    if(i == 1 || i == 4 || i == 7 || i == 10){
-        v_desejada = -K[i]*velocidade_roll;
-    }
-    else{
-            v_desejada = -K[i]*velocidade_pitch;
-        }
-
-    v_medicao_int = cmd.read_mov_speed(portHandler, packetHandler, i);
-    v_medicao = ler_velocidade(v_medicao_int);
-    v_aplicada = v_desejada - v_medicao;
-
-    cmd.write_mov_speed(portHandler, packetHandler, i, velocidade(2.2*v_aplicada));
-
-	i++;
-    }
-	i = 1;
-    
-    
-    
-    }
-
-int main(){
-
-    command cmd;
-    char *dev_name = (char*)DEVICENAME;
-    dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(1);
-    dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(dev_name);
-    dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, 30, 2);
-
-
-    clock_t tInicio, tFim;
-    float tDecorrido;
-    float tsim = 10; //tempo de simulacao em segundos
-    float tam = 100; //tempo de amostragem em milisegundos
-    float contador2 = 0;
-    int i = 1;
 
 
     //--------------------------Inicializacao------------------------------//
@@ -303,7 +140,44 @@ int main(){
     //----------------------Loop principal------------------------------------//
     while(tDecorrido < tsim*1000){
 	if(tDecorrido>contador2){
-    controle();
+
+
+    inicializacao();
+    medicao(angulos, USB);
+    roll_medido = angulos[0];
+    pitch_medido = angulos [1];
+
+    filtro(tam, fc, roll_medido, roll, &out);
+    velocidade_roll = (out - roll)*(PI/180)/(tam/1000);  //em rad/s
+    roll = out;
+    filtro(tam, fc, pitch_medido, pitch, &out);
+    velocidade_pitch = (out - pitch)*(PI/180)/(tam/1000);  //em rad/s
+    pitch = out;
+
+   if(abs(velocidade_roll)<threshold){velocidade_roll = 0;}
+   if(abs(velocidade_pitch)<threshold){velocidade_pitch = 0;}
+
+    printf("%f %f \n", velocidade_roll, velocidade_pitch);
+    i = 1;
+    while(i<13){
+    if(i == 1 || i == 4 || i == 7 || i == 10){
+        v_desejada = -K[i]*velocidade_roll;
+    }
+    else{
+            v_desejada = -K[i]*velocidade_pitch;
+        }
+
+    v_medicao_int = cmd.read_mov_speed(portHandler, packetHandler, i);
+    v_medicao = ler_velocidade(v_medicao_int);
+    v_aplicada = v_desejada - v_medicao;
+
+    cmd.write_mov_speed(portHandler, packetHandler, i, velocidade(2.2*v_aplicada));
+
+	i++;
+    }
+	i = 1;
+
+
     contador2 = contador2 + tam;
     }
 
@@ -313,7 +187,6 @@ int main(){
 
 
     //-----------Fim da simulacao, parar os motores -------------//
-    i=1;
     while(i<13){
 
     cmd.write_mov_speed(portHandler, packetHandler, i, velocidade(0));
