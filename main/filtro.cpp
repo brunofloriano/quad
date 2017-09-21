4a8c7aaef71472f7b8bc579b951149dc199c839e
Bruno Floriano <brunofloriano@github.com>
2017-09-21 14:48:00 -0300

    Filtro Velocidade

--- main/filtro.cpp
+++ main/filtro.cpp
@@ -5,7 +5,7 @@ float filtro(float tam, float fc, float Ak, float Afko, float *Afk){
     float a1 = 1/(1+T*2*pi*fc), a2 = T*2*pi*fc/(1+T*2*pi*fc);
 
     *Afk = a1*Afko + a2*Ak;
-    printf("%f \n",*Afk);
+    printf("%f \n",a1, a2, T,fc,Ak,Afko,*Afk);
 
 return 0;
 
