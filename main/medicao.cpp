d1f215bc50169a52110772adbfea0e827a1ec42a
Bruno Floriano <brunofloriano@github.com>
2017-09-21 09:24:04 -0300

    Problema Read

--- main/medicao.cpp
+++ main/medicao.cpp
@@ -76,7 +76,7 @@ int medicao(float *angulos, int USB){
             n_endl=0;
             memset(response, '\0', sizeof response);
 
-        select(USB+1, USB, NULL,NULL, 1);
+        select(USB+1, (fd_set *)USB, NULL,NULL, 1);
             n_written = write( USB, "1", 1 );
 
             do
