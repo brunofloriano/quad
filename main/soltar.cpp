2e27be027e4e3a5bdd5d6f724a52c03f63974fc4
Bruno Floriano <brunofloriano@github.com>
2017-10-03 08:46:08 -0300

    Controle Ganhos

---
+++ main/soltar.cpp
@@ -0,0 +1,45 @@
+#include "include/command.h"
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+
+#define BROADCASTID			            254
+
+using namespace std;
+
+int main(){
+    command cmd;
+    char *dev_name = (char*)DEVICENAME;
+    dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(1);
+    dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(dev_name);
+    dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, 30, 2);
+    
+    
+    
+       //--------------------------Inicializacao------------------------------//
+    if (portHandler->openPort())
+    {
+        printf("Succeeded to open the port!\n\n");
+        printf(" - Device Name : %s\n", dev_name);
+        printf(" - Baudrate    : %d\n\n", portHandler->getBaudRate());
+    }
+    else
+    {
+        printf("Failed to open the port! [%s]\n", dev_name);
+        printf("Press any key to terminate...\n");
+        cmd.getch();
+        return 0;
+    }
+
+    cmd.config_ram(portHandler, packetHandler);
+
+    for(i=0; i<12;i++)
+    {
+         cmd.write_mov_speed(portHandler, packetHandler, i+1, 0);
+    }
+    
+        cmd.write_torque(portHandler, packetHandler, BROADCASTID, 0);
+        
+        return 0;
+}
