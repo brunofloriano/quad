932c3ec152120a56c1dc4ffb4cd7371eb583032b
Bruno Floriano <brunofloriano@github.com>
2017-10-05 11:02:38 -0300

    Controle Ganhos

--- main/main.cpp
+++ main/main.cpp
@@ -48,7 +48,7 @@ void controle (union sigval sigval);
     float angulos[2];
     float fc = 1;
     float K_roll_R = 1.001, K_roll_L = 1.0;
-    float K_pitch_F = 1.01, K_pitch_R = 1.01;
+    float K_pitch_F = 1.11, K_pitch_R = 1.01;
     float K_UP = 1, K_DOWN = -1;
     float K[12];
     float threshold = 0;//0.0024;
