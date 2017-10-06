baa819c9bb6b710e7f638293c502b2858c5aba20
Bruno Floriano <brunofloriano@github.com>
2017-10-06 10:33:54 -0300

    Controle Ganhos

--- main/main.cpp
+++ main/main.cpp
@@ -146,7 +146,7 @@ void controle(union sigval arg){
     v_medicao_int = cmd.read_mov_speed(portHandler, packetHandler, i);
     v_medicao[i-1] = ler_velocidade(v_medicao_int);
 
-    v_aplicada = v_desejada - v_medicao[i-1];
+    v_aplicada = v_desejada - 1.5*v_medicao[i-1];
     if(i == 3 || i == 6 || i == 9 || i == 12){v_aplicada = 2.2*v_aplicada;}
     //else{v_aplicada = v_desejada - 1.1*v_medicao[i-1];}
 
