7a4e6b09ccecd849a57cbdd3766ab8d72dada895
Bruno Floriano <brunofloriano@github.com>
2017-10-09 14:30:33 -0300

    Controle Ganhos

--- main/medicao.cpp
+++ main/medicao.cpp
@@ -12,49 +12,6 @@
 
 using namespace std;
 
-int inicializacao(){
-
-    int USB = open( "/dev/ttyACM0", O_RDWR| O_NOCTTY | O_NONBLOCK);
-    close(USB);
-    USB = open( "/dev/ttyACM0", O_RDWR| O_NOCTTY | O_NONBLOCK);
-
-    //USB Handling//
-    memset (&tty, 0, sizeof tty);
-
-    /* Error Handling */
-    if ( tcgetattr ( USB, &tty ) != 0 )
-    {
-        //std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
-        printf("Erro from tcgetattr");
-    }
-
-    /* Save old tty parameters */
-    tty_old = tty;
-    /* Set Baud Rate */
-    cfsetospeed (&tty, (speed_t)B1000000);
-    cfsetispeed (&tty, (speed_t)B1000000);
-    /* Setting other Port Stuff */
-    tty.c_cflag     &=  ~PARENB;            // Make 8n1
-    tty.c_cflag     &=  ~CSTOPB;
-    tty.c_cflag     &=  ~CSIZE;
-    tty.c_cflag     |=  CS8;
-    tty.c_cflag     &=  ~CRTSCTS;           // no flow control
-    tty.c_cc[VMIN]   =  1;                  // read doesn't block
-    tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
-    tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
-    /* Make raw */
-    cfmakeraw(&tty);
-    /* Flush Port, then applies attributes */
-    //tcflush( USB, TCIFLUSH );
-    if ( tcsetattr ( USB, TCSANOW, &tty ) != 0)
-    {
-        //std::cout << "Error " << errno << " from tcsetattr" << std::endl;
-        printf("Erro from tcsetattr \n");
-    }
-
-return USB;
-}
-
 int medicao(float *angulos, int USB){
 
     char buf = '\0';
