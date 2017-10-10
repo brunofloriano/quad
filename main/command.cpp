7ef4e7005be4d18c9250deb0a24ac1e2f11ca99a
Bruno Floriano <brunofloriano@github.com>
2017-10-10 15:06:46 -0300

    Controle Ganhos

--- main/command.cpp
+++ main/command.cpp
@@ -62,19 +62,19 @@ void command::write_pos(dynamixel::PortHandler *portHandler, dynamixel::PacketHa
 int command::read_mov_speed(dynamixel::PortHandler *portHandler, dynamixel::PacketHandler *packetHandler, uint8_t id)
 {
 
-    uint8_t dxl_error = 0;
-    int     dxl_comm_result = COMM_TX_FAIL;
-    int16_t value16   = 0;
+//    uint8_t dxl_error = 0;
+//    int     dxl_comm_result = COMM_TX_FAIL;
+//    int16_t value16   = 0;
     //do{
-        dxl_comm_result=packetHandler->read2ByteTxRx(portHandler, id, 38,(uint16_t*)&value16,&dxl_error);  // Endereço p/posiçao atual 36,37
+//        dxl_comm_result=packetHandler->read2ByteTxRx(portHandler, id, 38,(uint16_t*)&value16,&dxl_error);  // Endereço p/posiçao atual 36,37
 //        if (dxl_comm_result == COMM_SUCCESS)
 //        {
 //           if (dxl_error != 0) packetHandler->printRxPacketError(dxl_error);
-            return(value16);
+//            return(value16);
 //        }
     //}while(dxl_comm_result != COMM_SUCCESS);
     //packetHandler->printTxRxResult(dxl_comm_result);
-    return(-1);
+//    return(-1);
 }
 
 void command::write_mov_speed(dynamixel::PortHandler *portHandler, dynamixel::PacketHandler *packetHandler, uint8_t id, uint32_t value)
